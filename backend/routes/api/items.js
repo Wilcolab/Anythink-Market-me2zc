aa5f84676f9af7f315abb25d81a01f0cb49119cb
JJ <jenkins@topscreenmedia.com>
2022-09-20 13:12:30 +0100

    fixed autocomplete error

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -53,7 +53,7 @@ router.get("/", auth.optional, function(req, res, next) {
     query.tagList = { $in: [req.query.tag] };
   }
 
-  if(typeof req.filter.title !== "undefined") {
+  if(typeof req.query.title !== "undefined") {
     query.title = req.filter.title;
   }
 

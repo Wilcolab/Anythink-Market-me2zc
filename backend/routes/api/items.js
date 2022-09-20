c4dee1716dfdb90502caafa525b6bce041597559
JJ <jenkins@topscreenmedia.com>
2022-09-20 13:20:00 +0100

    changed filter to regexp

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -54,7 +54,7 @@ router.get("/", auth.optional, function(req, res, next) {
   }
 
   if(typeof req.query.title !== "undefined") {
-    query.title = req.filter.title;
+    query.title = { $regex: req.query.title, $options: 'i' };
   }
 
   Promise.all([
